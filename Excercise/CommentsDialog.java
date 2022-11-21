8658f476d302e7d32f9a3121d661c783e1576c68
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-11-21 18:19:27 +0800

    Display types of comment in a dialog box

---
+++ Excercise/CommentsDialog.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+/** Programming comments are non-executing statements you add
+ to a program for the purpose of documentation  */
+class CommentsDialog {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog
+                (null, "Program comments are nonexecuting statements you add" +
+                        "\nto a program for the purpose of documentation.");
+        // line comments
+        // Program comments are nonexecuting statements you add
+        // to a program for the purpose of documentation.
+        // block comment
+       /* Program comments are non-executing statements you add
+          to a program for the purpose of documentation. */
+        // javadoc comment
+    }
+}
