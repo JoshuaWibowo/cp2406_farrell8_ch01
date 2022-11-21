50cee63280bfee28f85f2250d5d80612946a3935
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-11-21 18:11:17 +0800

    Display favorite song

---
+++ Excercise/FavoriteSong.java
@@ -0,0 +1,8 @@
+class FavoriteSong {
+    public static void main(String[] args){
+        System.out.println("Darling, darling, don't be scared");
+        System.out.println("'Cause even if I look everywhere");
+        System.out.println("Your colors caught my eye");
+        System.out.println("And you're my favorite sight to see");
+    }
+}
