7d8502fffcadc679061454bc00dc4e9131114c2a
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-11-21 18:15:40 +0800

    Comments class

---
+++ Excercise/Comments.java
@@ -0,0 +1,15 @@
+/** Program comments are nonexecuting statements
+ you add to a file for the purpose of documentation.  */
+class Comments {
+    public static void main(String[] args){
+        System.out.println("Program comments are nonexecuting statements ");
+        System.out.println("you add to a file for the purpose of documentation.");
+        // line comments
+        // Program comments are nonexecuting statements
+        // you add to a file for the purpose of documentation.
+        // block comment
+      /* Program comments are nonexecuting statements
+         you add to a file for the purpose of documentation. */
+        // javadoc comment
+    }
+}
