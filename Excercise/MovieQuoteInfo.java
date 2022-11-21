8614d82b6413632aac78f3c657dcf083792cfca6
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-11-21 15:21:16 +0800

    Display movie quote info

---
+++ Excercise/MovieQuoteInfo.java
@@ -0,0 +1,8 @@
+class MovieQuoteInfo {
+    public static void main(String[] args){
+        System.out.println("I once saw him kill three man in a bar with a pencil");
+        System.out.println("John Wick");
+        System.out.println("Viggo");
+        System.out.println("2014");
+    }
+}
