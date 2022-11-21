e5a9b0050cbdda9974161db5264c8d3d15641df4
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-11-21 15:15:13 +0800

    Display movie quote

---
+++ Excercise/MovieQuote.java
@@ -0,0 +1,6 @@
+class MovieQuote {
+    /* This program displays a movie quote */
+    public static void main(String[] arg) {
+        System.out.println("I once saw him kill three man in a bar with a pencil");
+    }
+}
